0fcac6f6fc86e4f797dd6e06ebf1a1522f0a2004
Ryan Ferreira <heyryanf@hotmail.com>
2026-07-09 22:01:38 -0300

    Reset: limpar site mantendo apenas Navbar e Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 import Navbar from "@/components/sections/Navbar";
-import Hero from "@/components/sections/Hero";
-import HowItWorks from "@/components/sections/HowItWorks";
-import Medal from "@/components/sections/Medal";
-import PricingLots from "@/components/sections/PricingLots";
-import FoundersWall from "@/components/sections/FoundersWall";
-import Faq from "@/components/sections/Faq";
 import Footer from "@/components/sections/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full bg-[#E3E2DE] text-[#141414]">
       <Navbar />
-      <Hero />
-      <HowItWorks />
-      <Medal />
-      <PricingLots />
-      <FoundersWall />
-      <Faq />
+
+      {/* Conteúdo novo será adicionado aqui */}
+
       <Footer />
     </main>
   );
