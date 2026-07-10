cb3e64616276b444e8b4768013336d45ad3cfaf0
Ryan Ferreira <heyryanf@hotmail.com>
2026-07-09 22:40:19 -0300

    Fix: build syntax error missing parenthesis in HowItWorks

--- src/components/sections/HowItWorks.tsx
+++ src/components/sections/HowItWorks.tsx
@@ -115,7 +115,7 @@ export default function HowItWorks() {
                   />
                 </motion.div>
               );
-            }))}
+            })}
 
             {/* Final checkmark dot */}
             <div className="relative flex justify-center">
